003baa41c647f23a69271404212f336ecdca84eb
ㅤㅤㅤ <64054647+horsehair@users.noreply.github.com>
2023-07-03 12:36:16 -0400

    uh

--- Cache/loader.lua
+++ Cache/loader.lua
@@ -1,57 +1,54 @@
 local Games = {
-    [2686040248] = "0bid0MathObby", --Math Obby By 0bid0
-    [9579974235] = "findthefloppas", --Find The Floppa Morphs
-    [10042496417] = "findthezombies", --Find The Zombie Morphs
-    [10955145868] = "findtheducks", --Find The Duck Morphs
-    [3587619225] = "megaeasyobby", --Mega Easy Obby
-    [9616411936] = "ManCityBlueMoon", --Man City Blue Moon
-    [12996397] = "megafunobby", --Mega Fun Obby
-    [9940051005] = "BloxLegends", --Blox Legends
-    [6403373529] = "slapbattles", --Slap Battles
-    [9757510382] = "dalyanstradesimulator", --Dylan's Trade Simulator
-    [8980446061] = "ultrabeauty", --Ultra Beauty
-    [9648883891] = "festivaltycoon", --Festive Tycoon
-    [9551640993] = "miningsimulator2", --Mining Simulator 2
-    [7952502098] = "ImpossibleGlassBridgeObby", --Impossible Glass Bridge Obby
-    [7378813434] = "cottonobby", --Cotton Obby
-    [9228830112] = "chocolateobby", --Chocolate Obby
-    [7341058233] = "FunObby", --Fun Obby
-    [9304358188] = "findthememes", --Find The Memes
-    [8328774140] = "dreamobby", --Dream Obby
-    [3327818733] = "timewastingsimulator", --Time Wasting Simulator
-    [11113551747] = "statgrindingsimulator2", --Stat Grinding Simulator 2
-    [11915606459] = "thedifficultymachine_Revamp", --The Defficulty Machine: Revamp
-    [10652184030] = "jumpclicker"; --Jump Clicker
+	[2686040248] = "0bid0MathObby",
+	[9579974235] = "findthefloppas",
+	[10042496417] = "findthezombies",
+	[10955145868] = "findtheducks",
+	[3587619225] = "megaeasyobby",
+	[9616411936] = "ManCityBlueMoon",
+	[12996397] = "megafunobby",
+	[9940051005] = "BloxLegends",
+	[6403373529] = "slapbattles",
+	[9757510382] = "dalyanstradesimulator",
+	[8980446061] = "ultrabeauty",
+	[9648883891] = "festivaltycoon",
+	[9551640993] = "miningsimulator2",
+	[7952502098] = "ImpossibleGlassBridgeObby",
+	[7378813434] = "cottonobby",
+	[9228830112] = "chocolateobby",
+	[7341058233] = "FunObby",
+	[9304358188] = "findthememes",
+	[8328774140] = "dreamobby",
+	[3327818733] = "timewastingsimulator",
+	[11113551747] = "statgrindingsimulator2",
+	[11915606459] = "thedifficultymachine_Revamp",
+	[13692314644] = "getcancelledtycoon",
+	[10652184030] = "jumpclicker"
 };
-
 local function Check()
-local Supported = Games[game.PlaceId] or false;
-if Supported ~= false then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/catgirIz/baddie/main/Cache/'..Supported..'.lua'))();
+	local Supported = Games[game.PlaceId] or false;
+	if Supported ~= false then
+		(loadstring(game:HttpGet("https://raw.githubusercontent.com/catgirIz/baddie/main/Cache/" .. Supported .. ".lua")))();
+	end;
+	if not game.PlaceId == Supported then
+		(game:GetService("StarterGui")):SetCore("SendNotification", {
+			Title = "Script Not Found",
+			Duration = "300",
+			Button1 = "Retry",
+			Text = "No Script For Game ID " .. game.PlaceId,
+			Icon = "rbxassetid://8447740059"
+		});
+	else
+		(game:GetService("StarterGui")):SetCore("SendNotification", {
+			Title = "Script Found!",
+			Duration = "300",
+			Text = "Found Script For Game ID " .. game.PlaceId,
+			Icon = "rbxassetid://8447740059"
+		});
+	end;
 end;
-
-if not game.PlaceId == Supported then
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Script Not Found",
-        Duration = "300";
-        Button1 = "Retry";
-        Text = "No Script For Game ID " .. game.PlaceId,
-        Icon = "rbxassetid://8447740059";
-    });
-else
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Script Found!",
-         Duration = "300";
-        Text = "Found Script For Game ID " .. game.PlaceId,
-        Icon = "rbxassetid://8447740059";
-    });
-end;
-    end;
-
 local function Callback(answer)
-    Check();
+	Check();
 end;
-
 local Bindable = Instance.new("BindableFunction");
 Bindable.OnInvoke = Callback;
 Check();
